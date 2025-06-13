44736734de15ca6f6303c432a0aeb48d22077d51
ishu <shresthaishu222@gmail.com>
2025-06-13 21:24:43 +0545

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let projectName = document.getElementById("projectName");
-let projectLink = document.getElementById("projectLink");
+// let projectName = document.getElementById("projectName");
+// let projectLink = document.getElementById("projectLink");
+
+let inputAdd = document.getElementsById("inputAdd");
+
+
 
 function addProject(){
+    let projectName = prompt("name");
+    let projectLink = prompt("link");
+
+    if (projectName.value === "" || projectLink.value === "") {
+        alert("Please fill in both fields.");
+    }
+
+
+    else{
     const div1 = document.createElement("div");
     div1.setAttribute("class","project-container");
 
     const anchor = document.createElement("a");
-    anchor.setAttribute("href", projectLink.value);
+    anchor.setAttribute("href", projectLink);
     anchor.setAttribute("target", "_blank");
     
     const button1 = document.createElement("button");
@@ -14,15 +27,40 @@ function addProject(){
 
     const h2 = document.createElement("h2");
     h2.setAttribute("class","name");
-    h2.innerHTML=projectName.value;
+    h2.innerHTML=projectName;
     
     const button2 = document.createElement("button");
-    button.setAttribute("class","delete");
-    button.setAttribute("onclick","deleteButton()");
-    button.innerHTML = "Delete";
+    button2.setAttribute("class","delete");
+    button2.setAttribute("onclick","deleteButton(this)");
+    button2.innerHTML = "Delete";
 
     button1.appendChild(h2);
     button1.appendChild(button2);
     anchor.appendChild(button1);
     div1.appendChild(anchor);
+
+    const container = document.querySelector(".project-container");
+    container.insertBefore(div1, container.lastElementChild);
+     
+    saveData();
+
+    // projectName.value = "";
+    // projectLink.value = "";
 }
+}
+
+function saveData()
+{
+    if(h2 || projectLink){
+localStorage.setItem("data",h2);
+localStorage.setItem("data",projectLink);
+
+}}
+function getData()
+{
+    if(h2 || projectLink){
+    h2.innerHTML=localStorage.getItem("data");
+    // projectLink.innerHTML=localStorage.getItem("data");
+
+}}
+getData();
