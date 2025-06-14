06df7a4448175a00d52a14e7309ef0c1a7a117fd
ishu <shresthaishu222@gmail.com>
2025-06-14 13:53:22 +0545

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // let projectName = document.getElementById("projectName");
 // let projectLink = document.getElementById("projectLink");
 
-let inputAdd = document.getElementsById("inputAdd");
+let inputAdd = document. getElementById("inputAdd");
 
 
 
@@ -9,7 +9,7 @@ function addProject(){
     let projectName = prompt("name");
     let projectLink = prompt("link");
 
-    if (projectName.value === "" || projectLink.value === "") {
+    if (projectName === "" || projectLink=== "") {
         alert("Please fill in both fields.");
     }
 
@@ -22,21 +22,22 @@ function addProject(){
     anchor.setAttribute("href", projectLink);
     anchor.setAttribute("target", "_blank");
     
-    const button1 = document.createElement("button");
-    button1.setAttribute("class","box");
+    const div2 = document.createElement("div");
+    div2.setAttribute("class","box");
 
     const h2 = document.createElement("h2");
     h2.setAttribute("class","name");
     h2.innerHTML=projectName;
+    // inputAdd=h2.innerHTML;
     
-    const button2 = document.createElement("button");
-    button2.setAttribute("class","delete");
-    button2.setAttribute("onclick","deleteButton(this)");
-    button2.innerHTML = "Delete";
+    // const button2 = document.createElement("button");
+    // button2.setAttribute("class","delete");
+    // button2.setAttribute("onclick","deleteButton()");
+    // button2.innerHTML = "Delete";
 
-    button1.appendChild(h2);
-    button1.appendChild(button2);
-    anchor.appendChild(button1);
+    div2.appendChild(h2);
+    // button1.appendChild(button2);
+    anchor.appendChild(div2);
     div1.appendChild(anchor);
 
     const container = document.querySelector(".project-container");
@@ -49,18 +50,34 @@ function addProject(){
 }
 }
 
-function saveData()
-{
-    if(h2 || projectLink){
-localStorage.setItem("data",h2);
-localStorage.setItem("data",projectLink);
+// function saveData()
+// {
+//     if(h2 || projectLink){
+// localStorage.setItem("data",h2);
+// localStorage.setItem("data",projectLink);
 
-}}
-function getData()
-{
-    if(h2 || projectLink){
-    h2.innerHTML=localStorage.getItem("data");
-    // projectLink.innerHTML=localStorage.getItem("data");
+// }}
+// function getData()
+// {
+//     if(h2 || projectLink){
+//     h2.innerHTML=localStorage.getItem("data");
+//     // projectLink.innerHTML=localStorage.getItem("data");
 
-}}
-getData();
+// }}
+// getData();
+function saveData() {
+    localStorage.setItem("projectList", inputAdd.innerHTML);
+}
+
+function getData() {
+    inputAdd.innerHTML = localStorage.getItem("projectList");
+}
+
+getData(); 
+
+
+function deleteButton(){
+    let projectContainer = document.querySelector(".project-container");
+    projectContainer.removeChild();
+    saveData();
+}
